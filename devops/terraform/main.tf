c400884485664e4a7d9a6b0bcfc19db47f241cd2
Sai-pragath <saipragath007@gmail.com>
2026-07-15 16:34:49 +0530

    Fix AWS region mapping and use dynamic AMI fetching

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -69,9 +69,26 @@ resource "aws_security_group" "devops_sg" {
   }
 }
 
+# Fetch the latest Ubuntu 22.04 AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 # EC2 Instance
 resource "aws_instance" "devops_server" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS in us-east-1 (update if different region)
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id     = data.aws_subnets.default.ids[0]
   
